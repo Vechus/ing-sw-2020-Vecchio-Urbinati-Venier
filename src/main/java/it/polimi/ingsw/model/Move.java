6c368b8bf88f29415afa28bcfe434731f9ff50ad
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-03-25 16:49:25 +0100

    [model] Move.java completed

--- src/main/java/it/polimi/ingsw/model/Move.java
+++ src/main/java/it/polimi/ingsw/model/Move.java
@@ -1,4 +1,27 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.util.Vector2;
+
 public class Move {
+    Vector2 initPos;
+    Vector2 finPos;
+    int heightDiff;
+
+    public Move(Vector2 initPos, Vector2 finPos, int heightDiff) {
+        this.initPos = initPos;
+        this.finPos = finPos;
+        this.heightDiff = heightDiff;
+    }
+
+    public Vector2 getInitPos() {
+        return initPos;
+    }
+
+    public Vector2 getFinPos() {
+        return finPos;
+    }
+
+    public int getHeightDiff() {
+        return heightDiff;
+    }
 }
--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw.model;
 
 public class Player {
+
 }
