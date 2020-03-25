ee13fba67030bb74d919b460baefc116571e787a
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-03-25 16:42:58 +0100

    [util] Vector2.java functions

--- src/main/java/it/polimi/ingsw/util/Vector2.java
+++ src/main/java/it/polimi/ingsw/util/Vector2.java
@@ -1,4 +1,52 @@
 package it.polimi.ingsw.util;
 
+import java.util.Objects;
+
 public class Vector2 {
+    private int x;
+    private int y;
+
+
+    public Vector2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector2)) return false;
+        Vector2 vector2 = (Vector2) o;
+        return getX() == vector2.getX() &&
+                getY() == vector2.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+
+    @Override
+    public String toString() {
+        return "Vector2{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
