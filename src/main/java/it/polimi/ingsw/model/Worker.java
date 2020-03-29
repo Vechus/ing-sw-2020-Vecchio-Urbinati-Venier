84643e9892aef81930ef79dab1881b215cea2c71
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-03-29 12:54:51 +0200

    [docs] added JavaDocs for Worker.java

--- src/main/java/it/polimi/ingsw/model/Worker.java
+++ src/main/java/it/polimi/ingsw/model/Worker.java
@@ -6,24 +6,45 @@ public class Worker {
     private Vector2 position;
     private Player owner;
 
+    /**
+     * Create a Worker object, to be places on the board afterwards. Its position is initialised to (-1,-1)
+     * @param owner Player who owns the Worker
+     */
     public Worker(Player owner){
         this.position = new Vector2(-1, -1);
         this.owner = owner;
     }
 
+    /**
+     * Create a Worker object, with given position
+     * @param position Vector2 position, the position to set the Worker to
+     * @param owner Player who owns the Worker
+     */
     public Worker(Vector2 position, Player owner) {
         this.position = position;
         this.owner = owner;
     }
 
+    /**
+     * Get the position of this Worker
+     * @return position of Worker
+     */
     public Vector2 getPosition() {
         return position;
     }
 
+    /**
+     * Sets the position of this Worker
+     * @param position Vector2 position to set
+     */
     public void setPosition(Vector2 position) {
         this.position = position;
     }
 
+    /**
+     * Gets the owner of this Worker
+     * @return owner Player who own this Worker
+     */
     public Player getOwner() {
         return owner;
     }
