4c1bc866c8e6808762e881ba45ffc0738093a1c6
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-03-29 13:19:27 +0200

    [docs] added JavaDocs for Move.java

--- src/main/java/it/polimi/ingsw/model/Move.java
+++ src/main/java/it/polimi/ingsw/model/Move.java
@@ -7,27 +7,40 @@ public class Move {
     private Vector2 finPos;
     private int heightDiff;
 
-    public Move(Vector2 initPos, Vector2 finPos) {
-        this.initPos = initPos;
-        this.finPos = finPos;
-        this.heightDiff = 0;
-    }
 
+    /**
+     * Create a Move object with initial position, final position and height difference
+     * @param initPos Vector2 initial position
+     * @param finPos Vector2 final position
+     * @param heightDiff int height difference
+     */
     public Move(Vector2 initPos, Vector2 finPos, int heightDiff) {
         this.initPos = initPos;
         this.finPos = finPos;
         this.heightDiff = heightDiff;
     }
 
+    /**
+     * Get initial position
+     * @return this.initPos
+     */
     public Vector2 getInitPos() {
-        return initPos;
+        return this.initPos;
     }
 
+    /**
+     * Get final position
+     * @return this.finPos
+     */
     public Vector2 getFinPos() {
-        return finPos;
+        return this.finPos;
     }
 
+    /**
+     * Get height difference
+     * @return this.heightDiff
+     */
     public int getHeightDiff() {
-        return heightDiff;
+        return this.heightDiff;
     }
 }
