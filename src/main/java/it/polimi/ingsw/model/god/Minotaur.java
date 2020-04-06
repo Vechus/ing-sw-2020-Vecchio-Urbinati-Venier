e73f3493b9c3918a4211e73a3e3b555132806b54
Daria Urbinati <daria.urbinati@mail.polimi.it>
2020-04-06 14:07:34 +0200

    implemented Triton

--- src/main/java/it/polimi/ingsw/model/god/Minotaur.java
+++ src/main/java/it/polimi/ingsw/model/god/Minotaur.java
@@ -1,4 +1,11 @@
 package it.polimi.ingsw.model.god;
 
+import it.polimi.ingsw.model.Board;
+import it.polimi.ingsw.model.Player;
+
 public class Minotaur extends God {
+
+    public Minotaur(Board board, Player player) {
+        super(board, player);
+    }
 }
--- src/main/java/it/polimi/ingsw/model/god/Triton.java
+++ src/main/java/it/polimi/ingsw/model/god/Triton.java
@@ -1,10 +1,65 @@
 package it.polimi.ingsw.model.god;
 
+import it.polimi.ingsw.model.Action;
 import it.polimi.ingsw.model.Board;
 import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.util.Vector2;
 
 public class Triton extends God {
     public Triton(Board board, Player player) {
         super(board, player);
+    }
+
+    boolean isTritonInPerimeter;
+
+    @Override
+    public  boolean chooseAction (Action action){
+        if(this.hasMoved ){
+            if(action.getType()==Action.ActionType.MOVE && isTritonInPerimeter==true){
+                if(move(action)){
+                    return true;
+                }
+            }
+            if(action.getType()==Action.ActionType.BUILD){
+                if(build(action)) {
+                    hasFinishedTurn=true;
+                    return true;
+                }
+            }else if(action.getType()==Action.ActionType.BUILD_DOME){
+                if(buildDome(action)) {
+                    hasFinishedTurn = true;
+                    return true;
+                }
+            }
+        }else if (action.getType()==Action.ActionType.MOVE) {
+            if (move(action)) {
+                chosenWorker=action.getWorker();
+                this.hasMoved = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean move(Action action) {
+
+        Vector2 currPos= action.getWorker().getPosition();
+        int heightDiff = this.board.getHeight(currPos)-this.board.getHeight(action.getTargetPos());
+        //action will be built probably in model
+
+        if(!this.isWorkersMoveValid(action)) {
+            return false;
+        }
+
+        this.board.moveWorker(action);
+        action.getWorker().setPosition(action.getTargetPos());
+
+        //special setting for triton
+        if(action.getWorker().getPosition().getY()==4 || action.getWorker().getPosition().getX()==4){
+            isTritonInPerimeter=true;
+        }
+        return true;
     }
 }
