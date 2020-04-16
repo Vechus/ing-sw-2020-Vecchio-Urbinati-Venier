209cd85e3dda22aba2efe975644311ce4d1c2ab5
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-04-16 11:49:56 +0200

    [client] Implemented simple Client

---
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.view.client.Client;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ClientApp {
+    private static final String ip = "127.0.0.1";
+    private static final int port = 27000;
+    public static void main(String[] args) {
+        Client client = new Client(ip, port);
+
+        try {
+            client.run(menu());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static boolean menu() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("SANTORINI OFFICIAL GAME SPONSORED BY Java 13©");
+        String ans = input.next();
+        System.out.println("Vuoi aprire una nuova partita?[si/no] ");
+        while(!ans.equals("si") && !ans.equals("no")) {
+            ans = input.next();
+            System.out.println("Errore input! Vuoi aprire una nuova partita?[si/no] ");
+        }
+        input.close();
+        return ans.equals("si");
+    }
+}
