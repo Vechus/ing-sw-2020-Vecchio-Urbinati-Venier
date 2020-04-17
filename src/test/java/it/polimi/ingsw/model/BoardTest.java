330195e442ea705a7884b66551b6d2ff2cd4651c
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-04-17 10:13:44 +0200

    [CLI] better Board print

--- src/test/java/it/polimi/ingsw/model/BoardTest.java
+++ src/test/java/it/polimi/ingsw/model/BoardTest.java
@@ -17,6 +17,15 @@ class BoardTest {
         p1.setPlayerColor(ConsoleColor.BLUE_UNDERLINED + ConsoleColor.BLUE_BOLD_BRIGHT);
         p2.setPlayerColor(ConsoleColor.GREEN_UNDERLINED + ConsoleColor.GREEN_BOLD_BRIGHT);
         p3.setPlayerColor(ConsoleColor.RED_UNDERLINED + ConsoleColor.RED_BOLD_BRIGHT);
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                board.setHeight(new Vector2(i,j), (int) ((Math.random()*10) % 4));
+            }
+        }
+        board.setComplete(new Vector2(0,0), true);
+        board.setComplete(new Vector2(0,4), true);
+        board.setComplete(new Vector2(4,3), true);
+        board.setHeight(new Vector2(4,3), 3);
         board.placeWorker(p1.getWorker(1), new Vector2(0,1));
         board.placeWorker(p1.getWorker(0), new Vector2(0,3));
         board.placeWorker(p2.getWorker(1), new Vector2(3,4));
