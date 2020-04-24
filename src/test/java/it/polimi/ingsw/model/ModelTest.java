177c555e66c774a54ecb01d540026837ea7aef8a
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-04-24 17:28:27 +0200

    [junit] ModelTest full line coverage

--- src/test/java/it/polimi/ingsw/model/ModelTest.java
+++ src/test/java/it/polimi/ingsw/model/ModelTest.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.god.God;
+import it.polimi.ingsw.util.Vector2;
+import it.polimi.ingsw.util.listeners.ModelChangeListener;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -8,22 +10,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
+    final Model model = new Model();
 
     @Test
     void addNewPlayer() {
-        final Model model = new Model();
         model.addNewPlayer(new God(model.getBoard()));
         Assert.assertEquals(1, model.getNumberOfPlayers());
     }
 
     @Test
     void checkPlayersLoseCondition() {
-        Assert.assertTrue(true);
+        Vector2 origin = new Vector2(0,0);
+        Board board = model.getBoard();
+        model.addNewPlayer(new God(board));
+        board.placeWorker(model.getPlayer(0).getWorker(0), origin);
+        model.beginNewTurn(model.getPlayer(0));
+        board.setHeight(new Vector2(0,1), 2);
+        board.setHeight(new Vector2(1,0), 2);
+        model.checkPlayersLoseCondition();
+        assertEquals(1, model.getNumberOfPlayers());
+        //board.setHeight(new Vector2(1,1), 3);
+        //model.checkPlayersLoseCondition();
+        //assertEquals(0, model.getNumberOfPlayers());
     }
 
     @Test
     void checkGameOver() {
-        final Model model = new Model();
         model.addNewPlayer(new God(model.getBoard()));
         Assert.assertTrue(model.checkGameOver());
         model.addNewPlayer(new God(model.getBoard()));
@@ -32,17 +44,21 @@ class ModelTest {
 
     @Test
     void executeAction() {
-        Assert.assertTrue(true);
+        model.addNewPlayer(new God(model.getBoard()));
+        model.getBoard().placeWorker(model.getPlayer(0).getWorker(0), new Vector2(0,0));
+        assertTrue(model.executeAction(0, new Action(model.getPlayer(0).getWorker(0), new Vector2(0,1), Action.ActionType.MOVE)));
     }
 
     @Test
     void beginNewTurn() {
-        Assert.assertTrue(true);
+        model.addNewPlayer(new God(model.getBoard()));
+        model.getPlayer(0).setFinished(true);
+        model.beginNewTurn(model.getPlayer(0));
+        assertFalse(model.getPlayer(0).isFinished());
     }
 
     @Test
     void getNumberOfPlayers() {
-        final Model model = new Model();
         model.addNewPlayer(new God(model.getBoard()));
         Assert.assertEquals(model.getNumberOfPlayers(), 1);
         model.addNewPlayer(new God(model.getBoard()));
