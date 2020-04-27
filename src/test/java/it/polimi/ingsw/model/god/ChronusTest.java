d0df878e85dfe51c7572a65d6fd462530ef8cdd0
Daniele Venier <danielevenier1998@gmail.com>
2020-04-27 16:43:40 +0200

    Merge branch 'master' of /home/venio/IdeaProjects/ing-sw-2020-Vecchio-Urbinati-Venier with conflicts.

--- src/test/java/it/polimi/ingsw/model/god/ChronusTest.java
+++ src/test/java/it/polimi/ingsw/model/god/ChronusTest.java
@@ -1,52 +1,38 @@
 package it.polimi.ingsw.model.god;
 
+import it.polimi.ingsw.model.Action;
 import it.polimi.ingsw.model.Board;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Worker;
 import it.polimi.ingsw.util.Vector2;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ChronusTest {
-    /*Board board;
+    Board board;
     Player player;
-    Vector2 highPos=new Vector2(1,0);;
-    Vector2 lowPos=new Vector2(1,1);;
-    Vector2 midPos=new Vector2(0,0);
-    Worker worker;
-    God god= new God(board, player);
+    Chronus chronus;
+    Worker playerWorker;
 
     @BeforeEach
-    void setup() {
+    void setup(){
         board = new Board();
         player = new Player();
-        board.setHeight(highPos, 2);
-        board.setHeight(lowPos, 0);
-        board.setHeight(midPos, 1);
-        board.placeWorker(worker, midPos);
-        worker = new Worker(midPos, player);
-        board.setHeight(4,3);
-        board.setComplete(4,true);
-        board.setHeight(4,3);
-        board.setComplete(4,true);
-        board.setHeight(4,3);
-        board.setComplete(4,true);
-        board.setHeight(4,3);
-        board.setComplete(4,true);
-        board.setHeight(4,3);
-        board.setComplete(4,true);
+        chronus = new Chronus(board, player);
+        playerWorker = new Worker(player);
+        board.placeWorker(playerWorker, new Vector2(0, 0));
+
+        for(int i=0;i<4;i++){
+            board.setHeight(new Vector2(0, i), 3);
+            board.setComplete(new Vector2(0, i), true);
+        }
     }
 
     @Test
-    void winConditionValid(){
-
-
+    void testWinFalse(){
+        Action action = new Action(playerWorker, new Vector2(0, 1), Action.ActionType.MOVE);
+        assertFalse(chronus.checkWinCondition(action));
     }
-    @Test
-    void winConditionNotVadid(){
-
-    }*/
 }
