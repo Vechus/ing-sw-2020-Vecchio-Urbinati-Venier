63e854eb95c8521ee5da71f525cc9b4f3dd7fa03
Daniele Venier <danielevenier1998@gmail.com>
2020-06-11 12:31:01 +0200

    Unified jars

---
+++ src/main/java/it/polimi/ingsw/SantoriniLauncher.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.server.Server;
+
+import java.io.IOException;
+
+public class SantoriniLauncher {
+    public static void main(String[] args) {
+        if(args.length > 1){
+            System.err.println("USAGE: java ClientApp.jar [--cli | --server]");
+            return;
+        }
+        boolean useCli = false;
+        boolean useServer = false;
+        for(String arg : args)
+            if(arg.equals("--cli")) useCli = true;
+            else if(arg.equals("--server")) useServer = true;
+            else System.err.println("Unrecognized arg: "+arg);
+
+        if(useServer){
+            try {
+                Server server = new Server();
+                server.run();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            Client remoteClient = new Client(useCli);
+            remoteClient.run();
+        }
+    }
+}
