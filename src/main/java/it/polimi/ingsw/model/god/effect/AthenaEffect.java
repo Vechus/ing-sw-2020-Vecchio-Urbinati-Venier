a118091a3841be90750f2e2f4aa4eb3bb13384ac
Daniele Venier <danielevenier1998@gmail.com>
2020-07-01 14:38:47 +0200

    Fix Athena effect being applied to build actions

--- src/main/java/it/polimi/ingsw/model/god/effect/AthenaEffect.java
+++ src/main/java/it/polimi/ingsw/model/god/effect/AthenaEffect.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.model.god.effect;
 import it.polimi.ingsw.util.Action;
 import it.polimi.ingsw.model.Board;
 import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.util.ActionType;
 import it.polimi.ingsw.util.Vector2;
 
 public class AthenaEffect extends OpponentEffect {
@@ -15,6 +16,6 @@ public class AthenaEffect extends OpponentEffect {
     public boolean checkOpponentAction(Action a) {
         Vector2 currPos= a.getWorker().getPosition();
         int heightDiff = this.board.getHeight(a.getTargetPos())-this.board.getHeight(currPos);
-        return heightDiff <= 0;
+        return a.getType() != ActionType.MOVE || heightDiff <= 0;
     }
 }
